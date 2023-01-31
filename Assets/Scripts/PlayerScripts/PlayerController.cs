bd662301275d55f2655b79d62e32f92611946eb8
Jimmy Haboon <101476866+Jhaboon21@users.noreply.github.com>
2023-01-30 23:09:14 -0500

    trying to make horn instrument

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -5,32 +5,39 @@ using UnityEngine.Events;
 
 public class PlayerController : MonoBehaviour
 {
-	public float speed;
+	public float speed;			//player speed and jump values
 	public float jumpForce;
+	public float hRadius;		//effect radius of horn
+	public float hPower;		//pushing force of horn
 	private float moveInput;
 
 	private Rigidbody2D rb;
 	private bool facingRight = true;
 
-	[SerializeField]
-	private bool isGrounded;
+	//checks whether player is touching ground
+	[SerializeField] private bool isGrounded;
+	[SerializeField] private bool isPushable;
 	public Transform groundCheck;
 	public float checkRadius;
 	public LayerMask whatIsGround;
+	public LayerMask whatIsPushable;
 
+	//jump checks and extra jump numbers
 	private int extraJump;
 	public int extraJumpValue;
 	private float jumpTimeCounter;
 	public float jumpTime;
-	[SerializeField]
-	private bool isJumping;
+	[SerializeField] private bool isJumping;
 
+	//Sounds
 	private AudioSource audioSrc;
-	[SerializeField]public AudioClip[] fluteSounds;
+	public AudioClip[] fluteSounds;	//array of different sound effects when using flute
+	//public AudioClip[] hornSounds;	//array of different sounds for horn
 	public static PlayerController Soundman;
-	[SerializeField] private int randomSound;
+	private int randomSound;
 
 	public bool hasFlute;
+	public bool hasHorn;
 
     void Start()
     {
@@ -38,7 +45,12 @@ public class PlayerController : MonoBehaviour
 		extraJump = extraJumpValue;
 		rb = GetComponent<Rigidbody2D>();
 		audioSrc = GetComponent<AudioSource>();
+
+		//create circle around player that effects physical objects
+		Vector2 explosionPos = transform.position;
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(explosionPos, hRadius);
     }
+	//plays a random sound within the flutesound array
 	public void PlayFluteSound()
     {
 		randomSound = Random.Range(0, 2);
@@ -47,7 +59,9 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+		//Check using small circle on player feet to check for ground layermask
 		isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+		isPushable = Physics2D.OverlapCircle(rb.position, hRadius, whatIsPushable);
 
 		moveInput = Input.GetAxis("Horizontal");
 		rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
@@ -64,16 +78,18 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-		if (isGrounded == true)
+		if (isGrounded == true) //touching the ground will reset jump amount
 			extraJump = extraJumpValue;
 		if (hasFlute == true)
 		{
+			//initialize jump when on ground
 			if (Input.GetButtonDown("Jump") && extraJump > 0 && isGrounded == true)
 			{
 				isJumping = true;
 				jumpTimeCounter = jumpTime;
 				rb.velocity = Vector2.up * jumpForce;
 			}
+			//while in the air with flute, perform double jump and sound
 			else if (Input.GetButtonDown("Jump") && extraJump > 0)
 			{
 				PlayFluteSound();
@@ -82,8 +98,10 @@ public class PlayerController : MonoBehaviour
 				rb.velocity = Vector2.up * jumpForce;
 				extraJump--;
 			}
+			//player can do a short or higher jump depending on if jump button is held down.
 			if (Input.GetButton("Jump") && isJumping == true)
 			{
+				//jump height is decided on a timer. if timer is above 0, then player can still rise higher
 				if (jumpTimeCounter > 0)
 				{
 					rb.velocity = Vector2.up * jumpForce;
@@ -92,13 +110,16 @@ public class PlayerController : MonoBehaviour
 				else
 					isJumping = false;
 			}
+			//when player lets go of jump button(even if jump timer != 0), then jump is stopped
 			if (Input.GetButtonUp("Jump"))
 			{
 				isJumping = false;
 			}
 		}
+		//jump is different when flute is not equipped.
 		else if (hasFlute == false)
 		{
+			//cannot double jump and jump is shorter.
 			if (Input.GetButtonDown("Jump") && isGrounded == true)
 			{
 				isJumping = true;
@@ -109,8 +130,17 @@ public class PlayerController : MonoBehaviour
 				isJumping = false;
 			}
 		}
+		if (hasHorn && isPushable)
+        {
+			if (Input.GetKeyDown(KeyCode.E))
+            {
+				Debug.Log("Used Horn");
+				
+            }
+        }
     }
 
+	//flip player sprite according to input direction
     void Flip()
     {
 		facingRight = !facingRight;
